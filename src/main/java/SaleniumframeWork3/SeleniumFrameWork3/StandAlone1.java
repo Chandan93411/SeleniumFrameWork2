f65e40566082a36ce57e7af8f33c114adfa1924e
chandan93411 <chandan93411@gmail.com>
2024-09-07 22:52:17 +0530

    First commit

--- src/main/java/SaleniumframeWork3/SeleniumFrameWork3/StandAlone1.java
+++ src/main/java/SaleniumframeWork3/SeleniumFrameWork3/StandAlone1.java
@@ -1,6 +1,11 @@
 package SaleniumframeWork3.SeleniumFrameWork3;
 
 public class StandAlone1 {
+
+
+    Public static void main(String args[]){
+        System.out.println("I am Gangster");
+    }
 	
 	
 
